6004de8e4105929e8ec7f1d0d91fbd88cc0f7ae4
sahithi0312 <61830891+sahithi0312@users.noreply.github.com>
2020-05-07 08:48:36 +0530

    Add files via upload

---
+++ librarymanagementsystem/src/main/java/com/capgemini/librarymanagementsystem/controller/LibraryController.java
@@ -0,0 +1,14 @@
+package com.capgemini.librarymanagementsystem.controller;
+
+public class LibraryController {
+	public static void main(String[] args) {
+
+		DummyController.doReg();
+	}
+
+
+	
+}
+
+
+
